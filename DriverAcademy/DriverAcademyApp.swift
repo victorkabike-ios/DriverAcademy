91634b60f9dc4d8b57ba848086f9a8c027cba998
Victor Kabike <tinotendakab@gmaail.com>
2023-10-15 11:18:53 +0200

    Initial Commit

---
+++ DriverAcademy/DriverAcademyApp.swift
@@ -0,0 +1,17 @@
+//
+//  DriverAcademyApp.swift
+//  DriverAcademy
+//
+//  Created by victor kabike on 2023/10/15.
+//
+
+import SwiftUI
+
+@main
+struct DriverAcademyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
